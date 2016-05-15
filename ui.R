3c63972cfb31a86bc3130d4cf5b77ba19b42c963
Vladimir Malinovskiy <vmalinovskiy2@gmail.com>
2016-05-15 18:46:02 +0200

    documentation added to the code

--- ui.R
+++ ui.R
@@ -1,6 +1,3 @@
-#You must also include enough documentation so that a novice user could use your application.
-#The documentation should be at the Shiny website itself. Do not post to an external link.
-
 library(shiny)
 shinyUI(pageWithSidebar(
         headerPanel("Temperature Converter"),
@@ -17,6 +14,17 @@ shinyUI(pageWithSidebar(
                 h4("Temperature Scale"),
                 verbatimTextOutput("oid1"),
                 h4("Temperature Value"),
-                verbatimTextOutput("oid2")
+                verbatimTextOutput("oid2"),
+                h3("Application Documentation"),
+                "This R application created to test basic features of the Shiny package. The application allows to select one of the temparature scales (Fahrenhiet or Celsius), to enter temperature value, and to get the result of the converion to another scale.",
+                h4("Input Elements"),
+                "There are 3 input elements:",
+                "Input temperature scale selector (by default: Fahrenhiet), ",
+                "Slider which allows to select temperature value from the range, and ",
+                "Textbox which allows to enter temperature value manually.",
+                "The Temperature Value textbox is set by the slider. However, the value entered manually does not change the slider.",
+                "The slider range is (-50, 50) degree Celsius and respective values Fahreheit (-58, 122). Such slider settings helps to demonstrate the difference between two temperature scales.",
+                h4("Output Elements"),
+                "The output is the Temperature Scale to which the conversion was made and the Temperature Value - result of the conversion."
         )
 ))
